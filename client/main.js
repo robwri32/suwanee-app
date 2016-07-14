692bbc66b76356ab9eef1204854d50230074f572
robwri32 <robwri32@gmail.com>
2016-07-14 08:02:20 -0400

    update from home cleaned up main.js no real progress

--- client/main.js
+++ client/main.js
@@ -3,19 +3,41 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
-
+// home
 Router.route('/', function () {
   this.layout('ApplicationLayout');
     this.render('Home');
-    HTTP.call( 'GET', 'http://suwaneemagazine.com//wp-json/wp/v2/posts', {}, function( error, response ) {
-
+// api call out
+    HTTP.call( 'GET', 'http://suwaneemagazine.com/wp-json/wp/v2/posts', {}, function( error, response ) {
    if (error) {
       console.log(error);
    } else {
       console.log(response);
-   }
+    var SuwMagData = response;
+    this.SuwMagData = new ReactiveVar();
+     }
+  });
+  // close api callout
 });
+// end home
+
+// working example
+
+Router.route('/header/', function () {
+  this.layout('ApplicationLayout');
+    this.render('header');
+    var Alphas = ['ALL',
+              'A', 'B', 'C', 'D','E', 'F',
+              'G', 'H', 'I', 'J','K', 'L',
+              'M', 'N', 'O', 'P','Q', 'R',
+              'S', 'T', 'U', 'V','W', 'X',
+              'Y', 'Z'];
+
+Template.header.alphabets = function() {
+  return Alphas;
+};
 });
+// close working example
 
 Router.route('/about/', function () {
   this.layout('ApplicationLayout');
@@ -29,6 +51,8 @@ Router.route('/leaderboard/', function () {
   this.layout('ApplicationLayout');
     this.render('leaderboard');
 });
+
+
 
 
 Router.route('/addpup/', function () {
